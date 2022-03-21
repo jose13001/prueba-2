ce2ef422383b24022ffe70bff69eeb66c650a386
jose13001 <josecarlos_01_30@unitec.edu>
2022-03-21 09:19:20 -0600

    addSong terminado...creo

--- src/p2prueba_2/ITunes.java
+++ src/p2prueba_2/ITunes.java
@@ -27,19 +27,66 @@ public class ITunes {
         songs = new RandomAccessFile(playlist+"/songs.itn","rw");
         Downloads = new RandomAccessFile(playlist+"/Downloads.itn","rw");
                
+        initCode();
     }
     
+    public void initCode()throws IOException{
+        if(songs.length()==0){
+            songs.writeInt(1);
+            songs.writeInt(1);
+        }
+    }
+    
+    
+    
     public int getCodigo(long offset)throws IOException{
-        codigos.seek(offset);
-        
-        return ;
+        codigos.seek(0);
+        int song=songs.readInt();
+        int downloads=songs.readInt();
+        if(offset==0){
+            songs.seek(0);
+            songs.writeInt(song+1);
+            return song;
+        }else if(offset==4){
+            songs.seek(0);
+            songs.skipBytes(4);
+            songs.writeInt(downloads+1);
+            return downloads;
+        }else{
+            return 0;
+        }
+       
+       
     }
     
     public void addSong(String nombre,String cantante,double precio)throws IOException{
         songs.seek(songs.length());
+        int codigo =getCodigo(0);
+        songs.writeInt(codigo);
         songs.writeUTF(nombre);
         songs.writeUTF(cantante);
         songs.writeDouble(precio);
+        songs.writeInt(0);
+        songs.writeInt(0);
+    }
+    
+    public void reviewSong(int code,int stars)throws IOException{
+        
+        
+    }
+    
+    public void downloadSong(int codeSong,String cliente){
+        
+        
+    }
+    
+    public void songs(String txtFile){
+        
+        
+    }
+    
+    public void infoSong(int codeSong){
+        
         
     }
    
